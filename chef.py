ed818457fafeb75d5233378b34040d4c4ae6d6f5
Umang Parmar <umangjparmar@gmail.com>
2018-09-07 14:09:48 +0530

    Added init command description

--- chef.py
+++ chef.py
@@ -4,12 +4,13 @@ from chef_helpers import api
 
 @click.group()
 def cli():
-    pass
+	pass
 
 @cli.command()
 def init():
-    if utils.check_if_initialized():
-        click.echo('You have already initialized the application.')
-        click.echo('Please run \"chef reinit\" to reinitialize the application.')
-    else:
-        api.new_oauth2_token()
+	"""Get and store tokens for api calls."""
+	if utils.check_if_initialized():
+		click.echo('You have already initialized the application.')
+    	click.echo('Please run \"chef reinit\" to reinitialize the application.')
+	else:
+		api.new_oauth2_token()
